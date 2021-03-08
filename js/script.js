f92e2580d0837a9c9f2979f979d278dd8c515482
vadymkostyuk <kostiukvadym22@gmail.com>
2021-03-09 00:35:06 +0200

    add db.json

--- js/script.js
+++ js/script.js
@@ -281,4 +281,9 @@ window.addEventListener('DOMContentLoaded', function() {
             closeModal();
         }, 4000);
     }
+
+    fetch('http://localhost:3000/menu')
+      .then(data => data.json())
+      .then(res => console.log(res))
+
 });
